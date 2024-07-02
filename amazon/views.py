17dba9b8b6c88adebf2b854c5ab5c2e4e3731134
ramkrushnasathe <167886834+ramkrushnasathe@users.noreply.github.com>
2024-07-03 01:12:45 +0530

    Update views.py
    update amazon view

--- amazon/views.py
+++ amazon/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'amazon/home.html')
+    return render(request,'home.html')
 
 def sell(request):
     return render(request,'amazon/sell.html')
